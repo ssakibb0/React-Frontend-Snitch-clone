147c7f344f1f0ac16109e799bfaf215efe5a09cb
Mohd Sakib Shaikh <mohd.sakib.shaikh@ibm.com>
2024-11-16 17:41:17 +0530

    created const folder

--- src/component/StepIntoSpolightSection.js
+++ src/component/StepIntoSpolightSection.js
@@ -77,47 +77,11 @@
 import React, { useEffect, useRef } from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import { products } from '../constant/StepIntoSpotlightSecton';
 import '../style/style.css'; // Ensure the path is correct 
 import '../style/shop.css'; // Ensure the path is correct
 
 const StepIntoSpotlightSection = () => {
-  const products = [
-    {
-      img1: "https://www.snitch.co.in/cdn/shop/files/IMG_0537.jpg?v=1695038114&width=1800",
-      img2: "https://www.snitch.co.in/cdn/shop/files/IMG_0541.jpg?v=1695038114&width=1800",
-      title: "Contrast White Low Top Sneaker",
-      price: "Rs. 2,190",
-      sizes: [40, 41, 42, 43, 44],
-    },
-    {
-      img1: "https://www.snitch.co.in/cdn/shop/files/IMG_0591.jpg?v=1695036991&width=1800",
-      img2: "https://www.snitch.co.in/cdn/shop/files/IMG_0595.jpg?v=1695037016&width=1800",
-      title: "Panelled White Low Top Sneakers",
-      price: "Rs. 2,190",
-      sizes: [40, 41, 42, 43, 44],
-    },
-    {
-      img1: "https://www.snitch.co.in/cdn/shop/files/IMG_0759.jpg?v=1695036990&width=1800",
-      img2: "https://www.snitch.co.in/cdn/shop/files/IMG_0756.jpg?v=1695036990&width=1800",
-      title: "Combined Piece White Sneaker",
-      price: "Rs. 2,190",
-      sizes: [40, 41, 42, 43, 44],
-    },
-    {
-      img1: "https://www.snitch.co.in/cdn/shop/files/IMG_0848.jpg?v=1695036641&width=1800",
-      img2: "https://www.snitch.co.in/cdn/shop/files/IMG_0846_1.jpg?v=1695638430&width=1800",
-      title: "Basic Blue Casual Sneaker",
-      price: "Rs. 2,190",
-      sizes: [40, 41, 42, 43, 44],
-    },
-    {
-      img1: "https://www.snitch.co.in/cdn/shop/files/IMG_0828.jpg?v=1695036582&width=1800",
-      img2: "https://www.snitch.co.in/cdn/shop/files/IMG_0825.jpg?v=1695036582&width=1800",
-      title: "Basic White Casual Sneaker",
-      price: "Rs. 2,190",
-      sizes: [40, 41, 42, 43, 44],
-    },
-  ];
 
   const handleSizeClick = (size, product) => {
     console.log(`Selected size ${size} for product ${product.title}`);
