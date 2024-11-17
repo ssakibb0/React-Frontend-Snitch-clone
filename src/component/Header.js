8a3cb2e19f7374f42606c6b1c87c12d6015b9484
Mohd Sakib Shaikh <mohd.sakib.shaikh@ibm.com>
2024-11-17 22:26:07 +0530

    added responsive in header section

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,108 +1,4 @@
 
-import React from 'react';
-import '../style/style.css';  // Ensure the path is correct
-
-// Swiper Imports (for background slider)
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
-
-// Import Swiper modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
-const Header = ({ toggleSidebar, toggleCartSection, toggleSearch }) => {
-    const handleMenuClick = () => { console.log('Menu clicked'); toggleSidebar(); };
-  return (
-    <div className="header_section">
-      <header>
-        <div className="header_site">
-          <div className="nav_section" id="nav_section">
-            <div className="page_width">
-              <div className="nav_flex_item">
-                <div className="menu_bar" onClick={handleMenuClick}>
-                  <i className="ri-menu-2-fill"></i>
-                </div>
-                <div className="logo">
-                  <a href="#">
-                    <img src="img/snichWhite.png" alt="logo" className="whiteimg" />
-                    <img src="img/_New Snitch logo.png" alt="logo" className="balckimg" />
-                  </a>
-                </div>
-                <div className="nav_link_site">
-                  <div className="link_icone hidesoicone">
-                    <a href="contact.html">
-                      <i className="ri-user-3-line"></i>
-                    </a>
-                  </div>
-                  <div className="link_icone" onClick={toggleSearch}>
-                    <i className="ri-search-line search-bar"></i>
-                  </div>
-                  <div className="link_icone hidesoicone wislbag">
-                    <i className="ri-heart-line bicon wish_cart" style={{ color: '#e30d23' }}></i>
-                    <span id="count1">0</span>
-                  </div>
-                  <div className="link_icone shopping-bag cart" onClick={toggleCartSection}>
-                    <i className="ri-shopping-bag-line"></i>
-                    <span id="count">0</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* Background Slider */}
-          <div className="background">
-            {/* Preloading effect */}
-            <div className="lds-roller" id="lds-rollerInner"></div>
-            <Swiper
-              className="mySwiper backgroun_wrapper"
-              spaceBetween={0}
-              slidesPerView={1}
-              loop={true}
-              autoplay={{
-                delay: 3000,
-                disableOnInteraction: false,
-              }}
-              pagination={{
-                clickable: true,
-              }}
-              navigation={false}
-              grabCursor={false}
-              modules={[Autoplay, Pagination, Navigation]}
-              data-aos="fade-left"
-            >
-              <SwiperSlide>
-                <img src="img/back555.webp" alt="bg2" className="showimge" />
-                <img src="img/backsmall5.webp" alt="bg2" className="showsmall showimge1" />
-                <video autoPlay muted loop>
-                  <source src="./back video/video1.mp4" type="video/mp4" className="videohide" />
-                </video>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="img/bg2.webp" alt="bg2" className="showimge" />
-                <img src="img/product/new-inss.webp" alt="bg2" className="showsmall showimge1" />
-                <video autoPlay muted loop>
-                  <source src="back video/video2.mp4" type="video/mp4" className="videohide" />
-                </video>
-              </SwiperSlide>
-              <SwiperSlide>
-                <img src="img/bg3.webp" alt="bg3" className="showimge" />
-                <img src="img/product/pro2.webp" alt="bg2" className="showsmall showimge1" />
-                <video autoPlay muted loop>
-                  <source src="back video/video3.mp4" type="video/mp4" className="videohide" />
-                </video>
-              </SwiperSlide>
-            </Swiper>
-          </div>
-        </div>
-      </header>
-    </div>
-  );
-};
-
-export default Header;
 // import React from 'react';
 // import '../style/style.css';  // Ensure the path is correct
 
@@ -178,38 +74,131 @@ export default Header;
 //               data-aos="fade-left"
 //             >
 //               <SwiperSlide>
-//                 <div className="header-slide-content">
-//                   <img src="img/back555.webp" alt="bg2" className="header-image" />
-//                   <img src="img/backsmall5.webp" alt="bg2" className="showsmall header-image" />
-//                   <video autoPlay muted loop className="header-video">
-//                     <source src="./back video/video1.mp4" type="video/mp4" />
-//                   </video>
-//                 </div>
+//                 <img src="img/back555.webp" alt="bg2" className="showimge" />
+//                 <img src="img/backsmall5.webp" alt="bg2" className="showsmall showimge1" />
+//                 <video autoPlay muted loop>
+//                   <source src="./back video/video1.mp4" type="video/mp4" className="videohide" />
+//                 </video>
 //               </SwiperSlide>
 //               <SwiperSlide>
-//                 <div className="header-slide-content">
-//                   <img src="img/bg2.webp" alt="bg2" className="header-image" />
-//                   <img src="img/product/new-inss.webp" alt="bg2" className="showsmall header-image" />
-//                   <video autoPlay muted loop className="header-video">
-//                     <source src="back video/video2.mp4" type="video/mp4" />
-//                   </video>
-//                 </div>
+//                 <img src="img/bg2.webp" alt="bg2" className="showimge" />
+//                 <img src="img/product/new-inss.webp" alt="bg2" className="showsmall showimge1" />
+//                 <video autoPlay muted loop>
+//                   <source src="back video/video2.mp4" type="video/mp4" className="videohide" />
+//                 </video>
 //               </SwiperSlide>
 //               <SwiperSlide>
-//                 <div className="header-slide-content">
-//                   <img src="img/bg3.webp" alt="bg3" className="header-image" />
-//                   <img src="img/product/pro2.webp" alt="bg2" className="showsmall header-image" />
-//                   <video autoPlay muted loop className="header-video">
-//                     <source src="back video/video3.mp4" type="video/mp4" />
-//                   </video>
+//                 <img src="img/bg3.webp" alt="bg3" className="showimge" />
+//                 <img src="img/product/pro2.webp" alt="bg2" className="showsmall showimge1" />
+//                 <video autoPlay muted loop>
+//                   <source src="back video/video3.mp4" type="video/mp4" className="videohide" />
+//                 </video>
+//               </SwiperSlide>
+//             </Swiper>
+//           </div>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// };
+
+// // export default Header;
+// import React from 'react';
+// import '../style/style.css';  // Ensure the path is correct
+
+// // Swiper Imports (for background slider)
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/swiper-bundle.css';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+// import 'swiper/css/autoplay';
+
+// // Import Swiper modules
+// import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+// const Header = ({ toggleSidebar, toggleCartSection, toggleSearch }) => {
+//   const handleMenuClick = () => { console.log('Menu clicked'); toggleSidebar(); };
+
+//   return (
+//     <div className="header_section">
+//       <header>
+//         <div className="header_site">
+//           <div className="nav_section" id="nav_section">
+//             <div className="page_width">
+//               <div className="nav_flex_item">
+//                 <div className="menu_bar" onClick={handleMenuClick}>
+//                   <i className="ri-menu-2-fill"></i>
 //                 </div>
+//                 <div className="logo">
+//                   <a href="#">
+//                     <img src="img/snichWhite.png" alt="logo" className="whiteimg" />
+//                     <img src="img/_New Snitch logo.png" alt="logo" className="balckimg" />
+//                   </a>
+//                 </div>
+//                 {/* Header Links */}
+//                 <div className="nav_link_site">
+//                 <div className="link_icone hidesoicone">
+//                        <a href="contact.html">
+//                        <i className="ri-user-3-line"></i>
+//                  </a>
+//                  </div>
+//                   {/* Search Bar Icon */}
+//                   <div className="link_icone" onClick={toggleSearch}>
+//                     <i className="ri-search-line search-bar"></i>
+//                   </div>
+//                   <div className="link_icone hidesoicone wislbag">
+//                     <i className="ri-heart-line bicon wish_cart" style={{ color: '#e30d23' }}></i>
+//                     <span id="count1">0</span>
+//                   </div>
+//                   <div className="link_icone shopping-bag cart" onClick={toggleCartSection}>
+//                     <i className="ri-shopping-bag-line"></i>
+//                     <span id="count">0</span>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           {/* Background Slider */}
+//           <div className="background">
+//             <div className="lds-roller" id="lds-rollerInner"></div>
+//             <Swiper
+//               className="mySwiper backgroun_wrapper"
+//               spaceBetween={0}
+//               slidesPerView={1}
+//               loop={true}
+//               autoplay={{
+//                 delay: 3000,
+//                 disableOnInteraction: false,
+//               }}
+//               pagination={{
+//                 clickable: true,
+//               }}
+//               navigation={false}
+//               grabCursor={false}
+//               modules={[Autoplay, Pagination, Navigation]}
+//               data-aos="fade-left"
+//             >
+//               <SwiperSlide>
+//                 <img src="img/back555.webp" alt="bg2" className="showimge" />
+//                 <img src="img/backsmall5.webp" alt="bg2" className="showsmall showimge1" />
+//                 <video autoPlay muted loop>
+//                   <source src="./back video/video1.mp4" type="video/mp4" className="videohide" />
+//                 </video>
 //               </SwiperSlide>
 //               <SwiperSlide>
-//                 <div className="header-slide-content">
-//                   <img src="img/serchCamera/produc/img1.webp" alt="bg3" className="header-image" />
-//                   {/* <img src="img/product/pro2.webp" alt="bg2" className="showsmall header-image" /> */}
-                  
-//                 </div>
+//                 <img src="img/bg2.webp" alt="bg2" className="showimge" />
+//                 <img src="img/product/new-inss.webp" alt="bg2" className="showsmall showimge1" />
+//                 <video autoPlay muted loop>
+//                   <source src="back video/video2.mp4" type="video/mp4" className="videohide" />
+//                 </video>
+//               </SwiperSlide>
+//               <SwiperSlide>
+//                 <img src="img/bg3.webp" alt="bg3" className="showimge" />
+//                 <img src="img/product/pro2.webp" alt="bg2" className="showsmall showimge1" />
+//                 <video autoPlay muted loop>
+//                   <source src="back video/video3.mp4" type="video/mp4" className="videohide" />
+//                 </video>
 //               </SwiperSlide>
 //             </Swiper>
 //           </div>
@@ -220,3 +209,133 @@ export default Header;
 // };
 
 // export default Header;
+
+import React from 'react';
+import '../style/style.css';  // Ensure the path is correct
+
+// Swiper Imports (for background slider)
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+// Import Swiper modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+const Header = ({ toggleSidebar, toggleCartSection, toggleSearch }) => {
+  const handleMenuClick = () => { console.log('Menu clicked'); toggleSidebar(); };
+
+  return (
+    <div className="header_section">
+      <header>
+        <div className="header_site">
+          <div className="nav_section" id="nav_section">
+            <div className="page_width">
+              <div className="nav_flex_item">
+                <div className="menu_bar" onClick={handleMenuClick}>
+                  <i className="ri-menu-2-fill"></i>
+                </div>
+                <div className="logo">
+                  <a href="#">
+                    <img src="img/snichWhite.png" alt="logo" className="whiteimg" />
+                    <img src="img/_New Snitch logo.png" alt="logo" className="balckimg" />
+                  </a>
+                </div>
+                {/* Header Links */}
+                <div className="nav_link_site">
+                  <div className="link_icone hidesoicone">
+                    <a href="contact.html">
+                      <i className="ri-user-3-line"></i>
+                    </a>
+                  </div>
+                  {/* Search Bar Icon */}
+                  <div className="link_icone" onClick={toggleSearch}>
+                    <i className="ri-search-line search-bar"></i>
+                  </div>
+                  <div className="link_icone hidesoicone wislbag">
+                    <i className="ri-heart-line bicon wish_cart" style={{ color: '#e30d23' }}></i>
+                    <span id="count1">0</span>
+                  </div>
+                  <div className="link_icone shopping-bag cart" onClick={toggleCartSection}>
+                    <i className="ri-shopping-bag-line"></i>
+                    <span id="count">0</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Background Slider */}
+          <div className="background">
+            <div className="lds-roller" id="lds-rollerInner"></div>
+            <Swiper
+              className="mySwiper backgroun_wrapper"
+              spaceBetween={0}
+              slidesPerView={1}
+              loop={true}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={false}
+              grabCursor={false}
+              modules={[Autoplay, Pagination, Navigation]}
+              data-aos="fade-left"
+            >
+              <SwiperSlide>
+                <img src="img/back555.webp" alt="bg2" className="showimge" />
+                <img src="img/backsmall5.webp" alt="bg2" className="showsmall showimge1" />
+                <video autoPlay muted loop>
+                  <source src="./back video/video1.mp4" type="video/mp4" className="videohide" />
+                </video>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="img/bg2.webp" alt="bg2" className="showimge" />
+                <img src="img/product/new-inss.webp" alt="bg2" className="showsmall showimge1" />
+                <video autoPlay muted loop>
+                  <source src="back video/video2.mp4" type="video/mp4" className="videohide" />
+                </video>
+              </SwiperSlide>
+              <SwiperSlide>
+                <img src="img/bg3.webp" alt="bg3" className="showimge" />
+                <img src="img/product/pro2.webp" alt="bg2" className="showsmall showimge1" />
+                <video autoPlay muted loop>
+                  <source src="back video/video3.mp4" type="video/mp4" className="videohide" />
+                </video>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+        </div>
+      </header>
+
+      {/* Sticky Bottom Bar (Visible only on mobile) */}
+      <div className="sticky-bottom-nav">
+        <div className="sticky-icons">
+          <div className="link_icone" onClick={toggleSearch}>
+            <i className="ri-search-line"></i>
+          </div>
+          <div className="link_icone">
+            <i className="ri-heart-line" style={{ color: '#e30d23' }}></i>
+            <span id="count1">0</span>
+          </div>
+          <div className="link_icone" onClick={toggleCartSection}>
+            <i className="ri-shopping-bag-line"></i>
+            <span id="count">0</span>
+          </div>
+          <div className="link_icone hidesoicone">
+              <a href="contact.html">
+                <i className="ri-user-3-line"></i>
+              </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
+
---
+++ src/constant/Reviews.js
@@ -0,0 +1,28 @@
+export const reviews = [
+    {
+      img: "https://cdn.shopify.com/s/files/1/0420/7073/7058/products/Snitch_23Oct21_2003_56x56.jpg?v=1666120853",
+      title: "Great product",
+      description: "I recently purchased the most comfortable and stylish t-shirt, and I couldn't be happier with my choice!",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0420/7073/7058/products/ADS_4e2c3799-37e0-4de0-8957-93b1f245abdd_56x56.jpg?v=1650379023",
+      title: "Great product",
+      description: "I recently purchased the most comfortable and stylish t-shirt, and I couldn't be happier with my choice!",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0420/7073/7058/products/Snitch_May22_9809_56x56.jpg?v=1652809014",
+      title: "Great product",
+      description: "I recently purchased the most comfortable and stylish t-shirt, and I couldn't be happier with my choice!",
+    },
+    {
+        img: "https://cdn.shopify.com/s/files/1/0420/7073/7058/products/ADS_4e2c3799-37e0-4de0-8957-93b1f245abdd_56x56.jpg?v=1650379023",
+        title: "Great product",
+        description: "I recently purchased the most comfortable and stylish t-shirt, and I couldn't be happier with my choice!",
+    },
+    {
+        img: "https://cdn.shopify.com/s/files/1/0420/7073/7058/products/Snitch_23Oct21_2003_56x56.jpg?v=1666120853",
+        title: "Great product",
+        description: "I recently purchased the most comfortable and stylish t-shirt, and I couldn't be happier with my choice!",
+      },
+    // Add more reviews here...
+  ];
